130b164f93a93578398cd66de57ddd9a5143d5b8
dogakanbilgiser <blackw1ng>
2022-03-26 16:10:43 -0400

    wrote the code for fisrt class

---
+++ src/day0/First.java
@@ -0,0 +1,26 @@
+package day0;
+
+public class First {
+
+
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("working...");
+    }
+
+
+
+    // extra line
+    // mdiaksfjsdif
+    //sdlkfpoklspdf
+    \
+
+
+
+
+
+
+
+
+    }
+
